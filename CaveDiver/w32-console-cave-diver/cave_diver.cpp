36736f325f83bba2dc052244c9dc7d1792a41a1e
feliposz <feliposz@gmail.com>
2019-09-23 17:38:17 -0300

    CaveDiver: Set console mode and print x, y function

--- CaveDiver/w32-console-cave-diver/cave_diver.cpp
+++ CaveDiver/w32-console-cave-diver/cave_diver.cpp
@@ -1,9 +1,33 @@
 #include "windows.h"
 #include <stdio.h>
 
+const int nWidth = 80;
+const int nHeight = 30;
+
+void printxy(const char *text, int col, int row)
+{
+    // escape sequence to set cursor position
+    printf("\x1B[%d;%dH%s", row, col, text);
+}
+
 int main()
 {
-    printf("Cave diver clone\n");
+    // Enable VT100 escape sequences
+    DWORD dwMode;
+    HANDLE hStdInput = GetStdHandle(STD_INPUT_HANDLE);
+    HANDLE hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    GetConsoleMode(hStdOutput, &dwMode);
+    SetConsoleMode(hStdOutput, dwMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+    GetConsoleMode(hStdInput, &dwMode);
+    SetConsoleMode(hStdInput, dwMode | ENABLE_VIRTUAL_TERMINAL_INPUT);
 
+    // Resize Console buffer/screen
+    SetConsoleScreenBufferSize(hStdOutput, { nWidth, nHeight });
+    SMALL_RECT rect = { 0, 0, nWidth - 1, nHeight - 1 };
+    SetConsoleWindowInfo(hStdOutput, true, &rect);
+
+    printxy("Cave Diver\n", nWidth / 2 - 5, nHeight / 2);
+
+    system("pause");
     return 0;
 }
