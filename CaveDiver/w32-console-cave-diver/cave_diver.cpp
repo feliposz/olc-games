16262989c0feadef957589ced2c543b071cb9564
feliposz <feliposz@gmail.com>
2019-09-23 18:27:46 -0300

    CaveDiver: Drawing cave walls and scrolling

--- CaveDiver/w32-console-cave-diver/cave_diver.cpp
+++ CaveDiver/w32-console-cave-diver/cave_diver.cpp
@@ -1,14 +1,14 @@
 #include "windows.h"
-#include <chrono>
 #include <stdio.h>
+#include <chrono>
 #include <thread>
 
 using namespace std;
 
-const int nWidth = 80;
-const int nHeight = 30;
+const int nScrWidth = 80;
+const int nScrHeight = 30;
 
-char sb[nWidth * nHeight];
+char sb[nScrWidth * nScrHeight];
 
 void printxy(const char *text, int col, int row)
 {
@@ -18,14 +18,14 @@ void printxy(const char *text, int col, int row)
 
 void sbxy(const char chr, int col, int row)
 {
-    sb[col + row * nWidth] = chr;
+    sb[col + row * nScrWidth] = chr;
 }
 
 void draw_player(int x)
 {
     printxy("O-V-O", x, 3);
     printxy(" O-O ", x, 4);
-    printxy("  V  ", x, 5);    
+    printxy("  V  ", x, 5);
 }
 
 int main()
@@ -40,23 +40,28 @@ int main()
     SetConsoleMode(hStdInput, dwMode | ENABLE_VIRTUAL_TERMINAL_INPUT);
 
     // Resize Console buffer/screen
-    SetConsoleScreenBufferSize(hStdOutput, { nWidth, nHeight });
-    SMALL_RECT rect = { 0, 0, nWidth - 1, nHeight - 1 };
+    SetConsoleScreenBufferSize(hStdOutput, { nScrWidth, nScrHeight });
+    SMALL_RECT rect = { 0, 0, nScrWidth - 1, nScrHeight - 1 };
     SetConsoleWindowInfo(hStdOutput, true, &rect);
 
     int nHealth = 1000;
     int nDistance = 0;
-    float fPosition = nWidth / 2;
+    float fPosition = nScrWidth / 2;
 
-    for (int y = 0; y < nHeight; y++) {
-        for (int x = 0; x < nWidth; x++) {
+    for (int y = 0; y < nScrHeight; y++) {
+        for (int x = 0; x < nScrWidth; x++) {
             sbxy(' ', x, y);
         }
-    }    
+    }
+
+    int nMaxRow = nScrHeight - 3;
+    int nCaveCol = nScrWidth / 2;
+    int nGapWidth = 15;
 
     while (nHealth >= 0) {
         this_thread::sleep_for(10ms);
 
+        // Handle player movement
         if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
             fPosition -= 1;
         }
@@ -65,21 +70,41 @@ int main()
             fPosition += 1;
         }
 
+        // Position to draw cave
+        nCaveCol = 1 + nScrWidth / 2 + (nScrWidth / 2 - nGapWidth - 2) * sin(nDistance * 0.01) + 1.0 * sin(nDistance * 0.2);
+
+        // Scrool screen buffer
+        memmove(sb, sb + nScrWidth, nScrWidth*nMaxRow);
+
+        // Draw cave segment (row) to screen buffer
+        for (int x = 0; x < nScrWidth; x++) {
+            if (x < nCaveCol || x > nCaveCol + nGapWidth) {
+                sbxy('.', x, nMaxRow);
+            }
+            else if (x == nCaveCol || x == nCaveCol + nGapWidth) {
+                sbxy('#', x, nMaxRow);
+            }
+            else {
+                sbxy(' ', x, nMaxRow);
+            }
+        }
+
+        // Draw buffer to screen
         printxy(sb, 1, 1);
 
         draw_player((int)fPosition);
 
-        printxy("< Cave Diver > - ", 1, nHeight - 2);
+        printxy("< Cave Diver > - ", 1, nScrHeight - 1);
         printf("Health: %4d - Distance travelled: %6d      ", nHealth, nDistance);
-       
+
         // Test
         nHealth--;
         nDistance++;
     }
 
-    printxy("Game Over! ", 1, nHeight);
+    printxy("Game Over! ", 1, nScrHeight);
     printf("Distance travelled: %d\n", nDistance);
-    
+
     printf("Press ENTER to quit!");
     while (true) {
         if (GetAsyncKeyState(VK_RETURN) & 0x8000) {
