4877ecfc0b65ed9554a7fb0d400599460b747af1
feliposz <feliposz@gmail.com>
2019-09-26 03:01:11 -0300

    Conway: Basic algorithm working

--- Conway/w32-conway-game-of-life/game_of_life.cpp
+++ Conway/w32-conway-game-of-life/game_of_life.cpp
@@ -15,7 +15,7 @@ class GameOfLife : public olcConsoleGameEngine {
         memset(m_output, 0, sizeof(int) * nSize);
 
         for (int i = 0; i < nSize; i++) {
-            m_state[i] = rand() % 2;
+            m_output[i] = rand() % 2;
         }
 
         return true;
@@ -25,11 +25,27 @@ class GameOfLife : public olcConsoleGameEngine {
     {
         int nSize = ScreenWidth() * ScreenHeight();
         for (int i = 0; i < nSize; i++) {
-            m_output[i] = m_state[i];
+            m_state[i] = m_output[i];
         }
 
-        for (int y = 0; y < ScreenHeight(); y++) {
-            for (int x = 0; x < ScreenWidth(); x++) {
+        auto cell = [&](int x, int y) {
+            return m_state[y * ScreenWidth() + x];
+        };
+
+        for (int y = 1; y < ScreenHeight()-1; y++) {
+            for (int x = 1; x < ScreenWidth()-1; x++) {
+                int nNeighbors =
+                    cell(x - 1, y - 1) + cell(x, y - 1) + cell(x + 1, y - 1) +
+                    cell(x - 1, y) + 0 + cell(x + 1, y) +
+                    cell(x - 1, y + 1) + cell(x, y + 1) + cell(x + 1, y + 1);
+
+                if (cell(x, y)) {
+                    m_output[y * ScreenWidth() + x] = nNeighbors == 2 || nNeighbors == 3;
+                }
+                else {
+                    m_output[y * ScreenWidth() + x] = nNeighbors == 3;
+                }
+
                 Draw(x, y, PIXEL_SOLID, m_output[y * ScreenWidth() + x] ? FG_WHITE : FG_BLACK);
             }
         }
