e23506d540e6ea3b6ae620e2f04f7b7d95968641
feliposz <feliposz@gmail.com>
2019-10-09 19:05:58 -0300

    ArcadeRacing: Drawing track curvature

--- ArcadeRacing/w32-arcade-racing/racing.cpp
+++ ArcadeRacing/w32-arcade-racing/racing.cpp
@@ -5,6 +5,7 @@ class RacingGame : public olcConsoleGameEngine
     float CarPos;
     float Distance;
     float Speed;
+    float Curvature;
 
     // Inherited via olcConsoleGameEngine
     virtual bool OnUserCreate() override
@@ -12,6 +13,7 @@ class RacingGame : public olcConsoleGameEngine
         CarPos = 0;
         Distance = 0;
         Speed = 0;
+        Curvature = 0;
         return true;
     }
 
@@ -29,13 +31,16 @@ class RacingGame : public olcConsoleGameEngine
 
         Distance += 50.0f * Speed * fElapsedTime;
 
+        // Test
+        Curvature = 0.5f;
+
         for (int y = 0; y < ScreenHeight() / 2; y++) {
             int row = y + ScreenHeight() / 2;
-            float Perspective = 0.1f + 0.9f * ((float)y / ScreenHeight() * 2.0f);
+            float Perspective = 0.1f + 0.8f * ((float)y / ScreenHeight() * 2.0f);
             for (int x = 0; x < ScreenWidth(); x++) {
                 short color = 0;
 
-                float RoadCenter = 0.5f;
+                float RoadCenter = 0.5f + Curvature * powf(1.0f - Perspective, 3);
                 float RoadWidth = 0.6f * Perspective;
                 float ClipWidth = 0.15f * Perspective;
 
