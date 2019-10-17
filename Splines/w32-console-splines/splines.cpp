7f8a34271b75081e84ad64ed2113ab72064c20c9
feliposz <feliposz@gmail.com>
2019-10-17 15:06:00 -0300

    Splines: Drawing control points

--- Splines/w32-console-splines/splines.cpp
+++ Splines/w32-console-splines/splines.cpp
@@ -1,13 +1,35 @@
 #include "olcConsoleGameEngine.h"
+#include <string>
+#include <vector>
+#include <utility>
+using namespace std;
 
 class CatmullRomSplines : public olcConsoleGameEngine
 {
+    const int MaxPoints = 5;
+    const int Padding = 10;
+    const int ControlSize = 3;
+
+    vector<pair<float, float>> Points;
+    int Selected = 0;
+
     virtual bool OnUserCreate() override
     {
+        for (int i = 0; i < MaxPoints; i++) {
+            Points.emplace_back(make_pair(Padding + i * ((ScreenWidth() - Padding)/MaxPoints), ScreenHeight() / 2));
+        }
+
         return true;
     }
+
     virtual bool OnUserUpdate(float fElapsedTime) override
     {
+        int n = 0;        
+        for (auto p : Points) {
+            Fill(p.first, p.second, p.first + ControlSize, p.second + ControlSize, PIXEL_SOLID, n == Selected ? FG_YELLOW : FG_RED);
+            DrawString(p.first + ControlSize / 2, p.second + ControlSize / 2, to_wstring(n), FG_WHITE);
+            n++;
+        }
         return true;
     }
 };
