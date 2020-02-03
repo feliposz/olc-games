4712c69f6fa76975c166419621ab130653fe879f
feliposz <feliposz@gmail.com>
2020-02-03 04:16:00 -0300

    Asteroids: Square asteroids

--- Asteroids/w32-console-asteroids/w32-console-asteroids/asteroids.cpp
+++ Asteroids/w32-console-asteroids/w32-console-asteroids/asteroids.cpp
@@ -1,14 +1,42 @@
+#include <vector>
 #include "olcConsoleGameEngine.h"
+
+using namespace std;
+
+struct GameObject {
+    float x, y;
+    float dx, dy;
+    int size;
+    float angle;
+};
 
 class AsteroidsGame : public olcConsoleGameEngine
 {
+private:
+    vector<GameObject> asteroids;
+
+public:
     virtual bool OnUserCreate() override
     {
+        asteroids.push_back({20.0f, 20.0f, 20.0f, 20.0f, 16, 0.0f});
         return true;
     }
 
     virtual bool OnUserUpdate(float fElapsedTime) override
     {
+        Fill(0, 0, ScreenWidth(), ScreenHeight(), ' ', FG_BLACK);
+
+        for (auto &a : asteroids) {
+            a.x += a.dx * fElapsedTime;
+            a.y += a.dy * fElapsedTime;
+
+            for (int x = 0; x < a.size; x++) {
+                for (int y = 0; y < a.size; y++) {
+                    Draw(x + a.x, y + a.y);
+                }
+            }
+        }
+
         return true;
     }
 };
