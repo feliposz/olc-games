ff84e80b89a90272cfecd1710b33360bae578e5e
feliposz <feliposz@gmail.com>
2020-02-04 16:59:14 -0300

    AStar: Initialize nodes and neighbors

--- AStar/w32-console-astar/astar.cpp
+++ AStar/w32-console-astar/astar.cpp
@@ -2,6 +2,8 @@
 #include "olcConsoleGameEngine.h"
 using namespace std;
 
+const int GridSize = 16;
+
 struct Node
 {
     bool visited;
@@ -15,11 +17,60 @@ struct Node
 
 class AstarGame : public olcConsoleGameEngine
 {
-    // Inherited via olcConsoleGameEngine
+    Node *Nodes;
+
     virtual bool OnUserCreate() override
     {
+        InitNodes();
+        UpdateNeighbors();
+
         return true;
     }
+
+    void InitNodes()
+    {
+        Nodes = new Node[GridSize * GridSize];
+
+        for (int x = 0; x < GridSize; x++) {
+            for (int y = 0; y < GridSize; y++) {
+                Node *n = &Nodes[y * GridSize + x];
+                n->global = INT32_MAX;
+                n->local = INT32_MAX;
+                n->x = x;
+                n->y = y;
+                n->visited = false;
+                n->obstacle = false;
+                n->parent = NULL;
+            }
+        }
+    }
+
+    void UpdateNeighbors()
+    {
+        for (int x = 0; x < GridSize; x++) {
+            for (int y = 0; y < GridSize; y++) {
+                Node *current = &Nodes[y * GridSize + x];
+                Node *left = &Nodes[y * GridSize + x - 1];
+                Node *right = &Nodes[y * GridSize + x + 1];
+                Node *up = &Nodes[(y - 1) * GridSize + x];
+                Node *down = &Nodes[(y + 1) * GridSize + x];
+                current->neighbors.clear();
+                if (x > 0 && !left->obstacle) {
+                    current->neighbors.push_back(left);
+                }
+                if (x < GridSize - 1 && !right->obstacle) {
+                    current->neighbors.push_back(right);
+                }
+                if (y > 0 && !up->obstacle) {
+                    current->neighbors.push_back(up);
+                }
+                if (y < GridSize - 1 && !down->obstacle) {
+                    current->neighbors.push_back(down);
+                }
+            }
+        }
+    }
+
     virtual bool OnUserUpdate(float fElapsedTime) override
     {
         return true;
