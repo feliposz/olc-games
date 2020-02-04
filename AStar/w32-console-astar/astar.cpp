b8e66bf422107ec3b702e4aad4635d6b98435f1d
feliposz <feliposz@gmail.com>
2020-02-04 16:38:35 -0300

    AStar: Node structure

--- AStar/w32-console-astar/astar.cpp
+++ AStar/w32-console-astar/astar.cpp
@@ -1,4 +1,17 @@
+#include <vector>
 #include "olcConsoleGameEngine.h"
+using namespace std;
+
+struct Node
+{
+    bool visited;
+    bool obstacle;
+    float x, y;
+    int local;
+    int global;
+    vector<Node *> neighbors;
+    Node *parent;
+};
 
 int main()
 {
