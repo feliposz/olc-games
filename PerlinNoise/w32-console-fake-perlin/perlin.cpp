b97ae9844e857def0c85f9bbeec194d26b37a71e
feliposz <feliposz@gmail.com>
2020-02-05 14:58:21 -0300

    PerlinNoise: Random seed

--- PerlinNoise/w32-console-fake-perlin/perlin.cpp
+++ PerlinNoise/w32-console-fake-perlin/perlin.cpp
@@ -3,9 +3,20 @@ using namespace std;
 
 class Game : public olcConsoleGameEngine
 {
+    float *RandomSeed = nullptr;
+    float *RandomNoise = nullptr;
+    int Size = 0;
 
     virtual bool OnUserCreate() override
     {
+        Size = ScreenWidth();
+        RandomSeed = new float[Size];
+        RandomNoise = new float[Size];
+
+        for (int i = 0; i < Size; i++) {
+            RandomSeed[i] = (float)rand() / RAND_MAX;
+        }
+
         return true;
     }
 
