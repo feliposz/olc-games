701c4eb8483342da648437ee7fba6028b427e66d
feliposz <feliposz@gmail.com>
2020-02-05 15:20:17 -0300

    PerlinNoise: Perlin noise 1D

--- PerlinNoise/w32-console-fake-perlin/perlin.cpp
+++ PerlinNoise/w32-console-fake-perlin/perlin.cpp
@@ -24,10 +24,7 @@ class Game : public olcConsoleGameEngine
     {
         Fill(0, 0, ScreenWidth(), ScreenHeight(), L' ');
 
-        // Test
-        for (int i = 0; i < Size; i++) {
-            RandomNoise[i] = RandomSeed[i];
-        }
+        PerlinNoise1D(Size, RandomSeed, 8, RandomNoise);
 
         for (int x = 0; x < ScreenWidth(); x++) {
             int y = (int)(RandomNoise[x] * ScreenHeight() / 2);
@@ -36,6 +33,27 @@ class Game : public olcConsoleGameEngine
 
         return true;
     }
+
+    void PerlinNoise1D(int size, float *seed, int octaves, float *noise)
+    {
+        for (int x = 0; x < size; x++) {
+            float scale = 1.0f;
+            float acc = 0.0f;
+            float n = 0.0f;
+            for (int o = 0; o < octaves; o++) {
+                int pitch = size >> o;
+                int sample1 = (x / pitch) * pitch;
+                int sample2 = (sample1 + pitch) % size;
+                float blend = (float)(x - sample1) / (float)pitch;
+                float interpolation = (1.0f - blend) * seed[sample1] + blend * seed[sample2];
+                n += interpolation * scale;
+                acc += scale;
+                scale /= 2.0f;
+            }
+            noise[x] = n / acc;
+        }
+    }
+
 };
 
 int main()
