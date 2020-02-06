e51d82289db2caee93386b86ddcc1cbbcf693596
feliposz <feliposz@gmail.com>
2020-02-06 01:52:45 -0300

    Worms: Perlin noise function

--- Worms/w32-console-worms/worms.cpp
+++ Worms/w32-console-worms/worms.cpp
@@ -1,7 +1,7 @@
 #include "olcConsoleGameEngine.h"
 using namespace std;
 
-class Game : public olcConsoleGameEngine
+class WormsGame : public olcConsoleGameEngine
 {
 
     virtual bool OnUserCreate() override
@@ -11,14 +11,40 @@ class Game : public olcConsoleGameEngine
 
     virtual bool OnUserUpdate(float fElapsedTime) override
     {
-        Fill(0, 0, ScreenWidth(), ScreenHeight(), L' ');
         return true;
+    }
+
+    void FillRandomSeed(int size, float *seed)
+    {
+        for (int i = 0; i < size; i++) {
+            seed[i] = (float)rand() / RAND_MAX;
+        }
+    }
+
+    void PerlinNoise1D(int size, float *seed, int octaves, float bias, float *noise)
+    {
+        for (int x = 0; x < size; x++) {
+            float scale = 1.0f;
+            float acc = 0.0f;
+            float n = 0.0f;
+            for (int o = 0; o < octaves; o++) {
+                int pitch = size >> o;
+                int sample1 = (x / pitch) * pitch;
+                int sample2 = (sample1 + pitch) % size;
+                float blend = (float)(x - sample1) / (float)pitch;
+                float interpolation = (1.0f - blend) * seed[sample1] + blend * seed[sample2];
+                n += interpolation * scale;
+                acc += scale;
+                scale /= bias;
+            }
+            noise[x] = n / acc;
+        }
     }
 };
 
 int main()
 {
-    Game game;
+    WormsGame game;
     game.ConstructConsole(256, 160, 6, 6);
     game.Start();
 
