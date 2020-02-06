724927bec9ab20ffb21e7abad85e95fecb5f9f55
feliposz <feliposz@gmail.com>
2020-02-06 02:18:40 -0300

    Worms: Map generation and camera

--- Worms/w32-console-worms/worms.cpp
+++ Worms/w32-console-worms/worms.cpp
@@ -3,15 +3,76 @@ using namespace std;
 
 class WormsGame : public olcConsoleGameEngine
 {
+    unsigned char *Map;
+    int MapWidth = 512;
+    int MapHeight = 256;
+    float CameraX = 0.0f;
+    float CameraY = 0.0f;
+    int CameraBorder = 10;
 
     virtual bool OnUserCreate() override
     {
+        Map = new unsigned char[MapWidth * MapHeight];
+        GenerateMap();
         return true;
     }
 
     virtual bool OnUserUpdate(float fElapsedTime) override
     {
+        float cameraSpeed = 400.0f;
+
+        if (m_mousePosX < CameraBorder) {
+            CameraX -= cameraSpeed * fElapsedTime;
+        }
+        if (m_mousePosY < CameraBorder) {
+            CameraY -= cameraSpeed * fElapsedTime;
+        }
+        if (m_mousePosX > ScreenWidth() - CameraBorder) {
+            CameraX += cameraSpeed * fElapsedTime;
+        }
+        if (m_mousePosY > ScreenHeight() - CameraBorder) {
+            CameraY += cameraSpeed * fElapsedTime;
+        }
+
+        if (CameraX < 0.0f) {
+            CameraX = 0.0f;
+        }
+        if (CameraX > MapWidth - ScreenWidth()) {
+            CameraX = MapWidth - ScreenWidth();
+        }
+        if (CameraY < 0.0f) {
+            CameraY = 0.0f;
+        }
+        if (CameraY > MapHeight - ScreenHeight()) {
+            CameraY = MapHeight - ScreenHeight();
+        }
+
+        for (int y = 0; y < ScreenHeight(); y++) {
+            for (int x = 0; x < ScreenWidth(); x++) {
+                short color = Map[((int)CameraY + y) * MapWidth + (int)CameraX + x] ? FG_CYAN : FG_DARK_GREEN;
+                Draw(x, y, PIXEL_SOLID, color);
+            }
+        }
+
         return true;
+    }
+
+    void GenerateMap()
+    {
+        float *Seed = new float[MapWidth];
+        float *Noise = new float[MapWidth];
+        FillRandomSeed(MapWidth, Seed);
+        Seed[0] = 0.5f;
+        PerlinNoise1D(MapWidth, Seed, 10, 2.0f, Noise);
+
+        for (int y = 0; y < MapHeight; y++) {
+            for (int x = 0; x < MapWidth; x++) {
+                Map[y * MapWidth + x] = Noise[x] * MapHeight > y ? 1 : 0;
+            }
+        }
+
+        delete[] Seed;
+        delete[] Noise;
     }
 
     void FillRandomSeed(int size, float *seed)
