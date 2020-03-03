8d8289c778fb92470918b880d1f877d4c0b351d1
feliposz <feliposz@gmail.com>
2020-03-03 05:08:47 -0300

    RPG: Basic sprite animation

--- RPG/rpg_dynamic.cpp
+++ RPG/rpg_dynamic.cpp
@@ -20,16 +20,68 @@ namespace Rpg {
 
     DynamicCreature::DynamicCreature(std::string name, olc::Sprite *sprite) : Dynamic(name)
     {
+        Health = 10;
+        MaxHealth = 10;
         m_sprite = sprite;
+        m_frame = 0;
+        m_timer = 0;
     }
 
     void DynamicCreature::Update(float elapsed)
     {
+        m_timer += elapsed;
+        if (m_timer > 0.2f) {
+            m_timer -= 0.2f;
+            m_frame = (m_frame + 1) % 2;
+        }
 
+        if (vx < -0.1f) {
+            m_direction = WEST;
+        }
+        if (vx > 0.1f) {
+            m_direction = EAST;
+        }
+        if (vy < -0.1f) {
+            m_direction = NORTH;
+        }
+        if (vy > 0.1f) {
+            m_direction = SOUTH;
+        }
+
+        if (Health <= 0) {
+            Health = 0;
+            m_state = DEAD;
+        }
+        else if (fabs(vx) > 0.1f || fabs(vy) > 0.1f) {
+            m_state = WALKING;
+        }
+        else {
+            m_state = STANDING;
+        }
     }
 
     void DynamicCreature::Draw(olc::PixelGameEngine *engine, float ox, float oy)
     {
-        engine->DrawPartialSprite((px - ox) * 16, (py - oy) * 16, m_sprite, 0, 0, 16, 16);
+        int spriteSheetX = 0;
+        int spriteSheetY = 0;
+        switch (m_state) {
+        case STANDING:
+            spriteSheetX = m_direction * 16;
+            spriteSheetY = 0 * 16;
+            break;
+        case WALKING:
+            spriteSheetX = m_direction * 16;
+            spriteSheetY = m_frame * 16;
+            break;
+        case CELEBRATING:
+            spriteSheetX = 4 * 16;
+            spriteSheetY = 0 * 16;
+            break;
+        case DEAD:
+            spriteSheetX = 4 * 16;
+            spriteSheetY = 1 * 16;
+            break;
+        }
+        engine->DrawPartialSprite((px - ox) * 16, (py - oy) * 16, m_sprite, spriteSheetX, spriteSheetY, 16, 16);
     }
 }
--- RPG/rpg_dynamic.h
+++ RPG/rpg_dynamic.h
@@ -27,8 +27,15 @@ namespace Rpg {
 
     protected:
         olc::Sprite *m_sprite;
+        enum { SOUTH = 0, WEST = 1, NORTH = 2, EAST = 3 } m_direction;
+        enum { STANDING, WALKING, CELEBRATING, DEAD } m_state;
+        int m_frame;
+        float m_timer;
 
     public:
+        float Health;
+        float MaxHealth;
+
         DynamicCreature(std::string name, olc::Sprite *sprite);
         void Update(float elapsed) override;
         void Draw(olc::PixelGameEngine *engine, float ox, float oy) override;
