a5aa303c6e6fade3e7644d7a77cc725423714fe6
feliposz <feliposz@gmail.com>
2021-03-04 04:17:15 -0300

    ConsoleWebCam: Added Console Game Engine, black/white threshold

--- ConsoleWebCam/webcam.cpp
+++ ConsoleWebCam/webcam.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <Windows.h>
 #include "escapi.h"
+#include "olcConsoleGameEngine.h"
 
 union Pixel
 {
@@ -8,51 +10,82 @@ union Pixel
     uint8_t c[4];
 };
 
+class WebCam : public olcConsoleGameEngine
+{
+private:
+    SimpleCapParams capture;
+    bool mirror = true;
+    bool flip = false;
+
+public:
+
+    virtual bool OnUserCreate() override
+    {
+        bool success = false;
+        int devices = setupESCAPI();
+
+        if (devices > 0)
+        {
+            capture.mWidth = ScreenWidth();
+            capture.mHeight = ScreenHeight();
+            Pixel *pixels = new Pixel[capture.mWidth * capture.mHeight];
+            capture.mTargetBuf = (int *)pixels;
+
+            success = initCapture(0, &capture);
+        }
+        return success;
+    }
+
+    virtual bool OnUserUpdate(float fElapsedTime) override
+    {
+        doCapture(0);
+        while (!isCaptureDone(0)) {}
+
+        if (GetKey('M').bPressed)
+        {
+            mirror = !mirror;
+        }
+
+        if (GetKey('V').bPressed)
+        {
+            flip = !flip;
+        }
+
+        for (int y = 0; y < ScreenHeight(); y++)
+        {
+            for (int x = 0; x < ScreenWidth(); x++)
+            {
+                int offsetX = mirror ? (capture.mWidth - x - 1) : x;
+                int offsetY = (flip ? (capture.mHeight - y - 1) : y) * capture.mWidth;
+                Pixel *p = (Pixel *)capture.mTargetBuf + offsetX + offsetY;
+                short sym, fg, bg;
+                BlackWhite(p->c[2], p->c[1], p->c[0], sym, fg, bg);
+                Draw(x, y, sym, fg | bg);
+            }
+        }
+
+        return true;
+    }
+
+    void BlackWhite(int r, int g, int b, short &sym, short &fg, short &bg)
+    {
+        sym = PIXEL_SOLID;
+        fg = g >= 127 ? FG_WHITE : FG_BLACK;
+        bg = BG_BLACK;
+    }
+
+    ~WebCam()
+    {
+        delete capture.mTargetBuf;
+        deinitCapture(0);
+    }
+};
+
 int main()
 {
-    int devices = setupESCAPI();
-
-    if (devices > 0)
-    {
-        SimpleCapParams capture;
-        capture.mWidth = 320;
-        capture.mHeight = 240;
-        Pixel *pixels = new Pixel[capture.mWidth * capture.mHeight];
-        char out[80 * 24 + 1];
-        capture.mTargetBuf = (int *)pixels;
-
-        if (initCapture(0, &capture))
-        {
-            for (;;)
-            {
-                doCapture(0);
-                while (!isCaptureDone(0)) {}
-
-                char *ptr = out;
-                for (int y = 0; y < 24; y++)
-                {
-                    for (int x = 0; x < 79; x++)
-                    {
-                        Pixel *p = pixels + (y*10) * capture.mWidth + (x*4);
-                        *ptr++ = p->c[1] >= 127 ? '.' : '@';
-                    }
-                    *ptr++ = '\n';
-                }
-                printf("%s\n", out);
-            }
-            out[sizeof(out) - 1] = 0;
-            deinitCapture(0);
-        }
-        else
-        {
-            printf("Capture failed - device may already be in use.\n");
-        }
-
-    }
-    else
-    {
-        printf("error initializing escapi or no camera present");
-    }
+    WebCam game;
+    game.ConstructConsole(320, 240, 4, 4);
+    game.Start();
 
     return 0;
 }
